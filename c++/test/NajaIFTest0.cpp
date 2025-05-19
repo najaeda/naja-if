01a9e96c9ff03f27b821fe7284a9df87c36ed86f
xtof <christophe.alex@gmail.com>
2025-05-19 18:40:28 +0200

    refactor code

--- c++/test/NajaIFTest0.cpp
+++ c++/test/NajaIFTest0.cpp
@@ -10,7 +10,7 @@
 #include <capnp/message.h>
 #include <capnp/serialize-packed.h>
 
-#include "snl_interface.capnp.h"
+#include "naja_nl_interface.capnp.h"
 
 #ifndef NAJA_IF_TEST_PATH
 #define NAJA_IF_TEST_PATH "Undefined"
@@ -32,29 +32,29 @@ TEST(NajaIFTest, test0) {
     auto library0InterfaceBuilder = libraries[0];
     library0InterfaceBuilder.setId(0);
     library0InterfaceBuilder.setName("LIB0");
-    auto designs = library0InterfaceBuilder.initDesignInterfaces(1);
+    auto designs = library0InterfaceBuilder.initSnlDesignInterfaces(1);
     auto design0InterfaceBuilder = designs[0];
     design0InterfaceBuilder.setId(0);
     design0InterfaceBuilder.setName("DESIGN0");
-    design0InterfaceBuilder.setType(DBInterface::LibraryInterface::DesignType::STANDARD);
+    design0InterfaceBuilder.setType(DesignType::STANDARD);
     auto terms = design0InterfaceBuilder.initTerms(3);
     auto term0InterfaceBuilder = terms[0];
     auto scalarTerm0 = term0InterfaceBuilder.initScalarTerm();
     scalarTerm0.setId(0);
     scalarTerm0.setName("TERM0");
-    scalarTerm0.setDirection(DBInterface::LibraryInterface::DesignInterface::Direction::INPUT);
+    scalarTerm0.setDirection(Direction::INPUT);
     auto term1InterfaceBuilder = terms[1];
     auto scalarTerm1 = term1InterfaceBuilder.initScalarTerm();
     scalarTerm1.setId(1);
     scalarTerm1.setName("TERM1");
-    scalarTerm1.setDirection(DBInterface::LibraryInterface::DesignInterface::Direction::OUTPUT);
+    scalarTerm1.setDirection(Direction::OUTPUT);
     auto term2InterfaceBuilder = terms[2];
     auto busTerm2 = term2InterfaceBuilder.initBusTerm();
     busTerm2.setId(2);
     busTerm2.setName("TERM2");
     busTerm2.setMsb(31);
     busTerm2.setLsb(0);
-    busTerm2.setDirection(DBInterface::LibraryInterface::DesignInterface::Direction::INOUT);
+    busTerm2.setDirection(Direction::INOUT);
 
 
     int fd = open(
@@ -77,5 +77,5 @@ TEST(NajaIFTest, test0) {
   auto library0Interface = dbInterface.getLibraryInterfaces()[0];
   EXPECT_EQ(0, library0Interface.getId());
   EXPECT_EQ("LIB0", library0Interface.getName());
-  EXPECT_EQ(1, library0Interface.getDesignInterfaces().size());
+  EXPECT_EQ(1, library0Interface.getSnlDesignInterfaces().size());
 }
